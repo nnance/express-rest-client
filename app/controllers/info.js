1e091b587f16024672fa44fa56f880c7ef58f38c
Nick Nance <nicknance@Nicks-MacBook-Pro-15i.local>
2013-12-30 07:39:34 -0600

    added rest support

--- app/controllers/info.js
+++ app/controllers/info.js
@@ -1,13 +1,21 @@
-var layout = require('../views/layout');
+var layout = require('../views/layout'),
+	rest = require('restler');
 
 exports.pages = function(req, res){
-	var model = {
-		title: 'Generator-Express MVC'
+	function render(model) {
+		layout.render(req, res, function(err, $){
+			res.render('info/' + req.params[0], model, function(err, html) {
+				$('body').append(html);
+				res.send($.html());
+		    });		
+		});
 	};
-	layout.render(req, res, function(err, $){
-		res.render('info/' + req.params[0], model, function(err, html) {
-			$('body').append(html);
-			res.send($.html());
-	    });		
+
+	rest.get('http://loopthread.appspot.com/rest/calendars/agxzfmxvb3B0aHJlYWRyKgsSD0NhbGVuZGFyQ2F0YWxvZxjxqAIMCxINRXZlbnRDYWxlbmRhchgCDA/feed').on('complete', function(data) {
+		var model = {
+			title: 'Generator-Express MVC',
+			events: data
+		};
+		render(model);
 	});
 };
