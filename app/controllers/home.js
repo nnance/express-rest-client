26e87fc391783801d3c506ecad2a7bbcf24c02d0
Nick Nance <nicknance@Nicks-MacBook-Pro-15i.local>
2013-12-29 20:16:08 -0600

    underscore template initial working

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,12 +1,19 @@
 var mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+	cheerio = require('cheerio'),
+	_ = require('underscore'),
+	fs = require('fs'),
+    tpl = fs.readFileSync( 'app/views/home/index.html' ).toString();
+    template = _.template(tpl);
+	Article = mongoose.model('Article');
 
 exports.index = function(req, res){
   Article.find(function(err, articles){
     if(err) throw new Error(err);
-    res.render('home/index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
+    $ = cheerio.load(template({title: 'Generator'}));
+    res.send($.html());
+    // res.render('home/index', {
+    //   title: 'Generator-Express MVC',
+    //   articles: articles
+    // });
   });
 };
